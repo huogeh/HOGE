f387bb8c67c5cda78447111e2afaa21ed382b621
huogeh <3123301762@qq.com>
2025-06-05 17:24:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log('Main JS Loaded');
+document.getElementById("openGameSelector").addEventListener("click", () => {
+  document.getElementById("gameSelectorModal").classList.remove("hidden");
+});
+document.getElementById("closeGameSelector").addEventListener("click", () => {
+  document.getElementById("gameSelectorModal").classList.add("hidden");
+});
